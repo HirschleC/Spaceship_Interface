4d65d4e4cd3f5bafaebaa1eacd9b978d8053318f
HirschleC <christian@hirschle.ch>
2019-05-04 17:12:36 +0200

    initial commit
      Initial commit
    
      Initial commit

---
+++ Spaceship_Interface.ino
@@ -0,0 +1,66 @@
+int switchState = 0;
+int StartProgramm = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(3, OUTPUT);
+ pinMode(4, OUTPUT);
+ pinMode(5, OUTPUT);
+ pinMode(2, INPUT);
+}
+
+void loop() {
+ // put your main code here, to run repeatedly:
+
+if (StartProgramm == 1) {
+ switchState = digitalRead(2);
+ if (switchState == LOW) {
+ // the button is not pressed
+  digitalWrite(3, HIGH); // green LED
+  digitalWrite(4, LOW);  // red LED
+  digitalWrite(5, LOW);  // red LED
+  }
+ else {
+ // the button is pressed
+  digitalWrite(3, LOW);  // green LED
+  digitalWrite(4, HIGH); // red LED
+  digitalWrite(5, LOW); // red LED
+  delay(250); // 1000 = 1 Sekunde
+  // toggle LEDs
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  delay(250);
+  }  
+}
+
+else {
+ switchState = digitalRead(2);
+ if (switchState == LOW) {
+  digitalWrite(3, LOW);  // green LED
+  digitalWrite(4, LOW); // red LED
+  digitalWrite(5, HIGH); // red LED
+  delay(250); // 1000 = 1 Sekunde
+  // toggle LEDs
+  digitalWrite(3, LOW);  // green LED
+  digitalWrite(4, HIGH); // red LED
+  digitalWrite(5, LOW); // red LED
+  delay(250); // 1000 = 1 Sekunde
+  digitalWrite(3, HIGH);  // green LED
+  digitalWrite(4, LOW); // red LED
+  digitalWrite(5, LOW); // red LED
+  delay(250);
+  }
+ else {
+ // the button is pressed
+  digitalWrite(3, LOW);  // green LED
+  digitalWrite(4, LOW); // red LED
+  digitalWrite(5, LOW); // red LED
+  delay(100); // 1000 = 1 Sekunde
+  // toggle LEDs
+  digitalWrite(3, HIGH);  // green LED
+  digitalWrite(4, HIGH); // red LED
+  digitalWrite(5, HIGH); // red LED
+  delay(100); // 1000 = 1 Sekunde
+  }
+ }
+}
